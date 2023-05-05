fe75da2d078b6e27d866a47d8faba547ee4d03d2
= <=>
2023-05-05 22:07:48 +0500

    Initial commit

--- src/Lab5/Example4.java
+++ src/Lab5/Example4.java
@@ -2,33 +2,10 @@ package Lab5;
 
 public class Example4 {
     public static void main(String[] args) throws InterruptedException {
-        var a = new int[2];
+        // С помощью цикла создаем нужное колличество потоков
         for (int i = 0; i < 10; i++) {
             Thread t1 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-
             t1.start();
-
         }
-//        Thread t1 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t2 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t3 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t4 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t5 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t6 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t7 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t8 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t9 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//        Thread t10 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-//
-//        t1.start();
-//        t2.start();
-//        t3.start();
-//        t4.start();
-//        t5.start();
-//        t6.start();
-//        t7.start();
-//        t8.start();
-//        t9.start();
-//        t10.start();
     }
 }
